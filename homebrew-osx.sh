1d2132fe6a33c28187cda7719f85a754383c056b
jackshutske <jackshutske@jackshutske.local>
2018-11-08 14:24:47 -0600

    -initial script -basic casks -various packages -homebrew updates, bash, etc

---
+++ homebrew-osx.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+# 
+# script for setting up a new OSX machine
+# 
+# free to run multiple times without issues
+#
+
+
+echo "starting script..."
+
+# check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# update homebrew recipes
+echo "updating homebrew..."
+brew update
+
+# install bash 4
+echo "installing bash..."
+brew install bash
+
+# install various packages
+packages=(
+    ffmpeg
+    wget
+    npm
+    ssh-copy-id
+    vim
+)
+
+
+echo "installing various packages..."
+brew install ${packages[@]}
+
+echo "cleaning time!!!"
+brew cleanup
+
+# install casks/applications
+echo "all aboard!!!"
+casks=(
+    avast-security
+    dashlane
+    discord
+    firefox
+    google-chrome
+    iterm2
+    malwarebytes
+    messenger
+    microsoft-office
+    slack
+    spotify
+    steam
+    visual-studio-code  
+)
+
+brew cask install ${casks[@]}
+
+# install ruby gems
+echo "installing ruby gems..."
+rubygems=(
+    bundler
+)
+sudo gem install ${rubygems[@]}
+
+echo "we're done..."
+echo "good-bye!!!"
